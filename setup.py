9f655eea8fcfd9571d730c8e9ed602f487ba497f
wattsmj <watmh@santos.com>
2019-05-14 11:37:50 +1000

    Added license header to source files

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+'''
+Smart Bookmark Tool that redirects your web browser based on labels
+Copyright (C) 2019  Matthew Watts
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Author: wattsmj
+Contact: Please raise an issue on www.github.com/wattsmj/django-aka
+Description: Package installation file
+'''
+
 import os
 from setuptools import find_packages, setup
 
